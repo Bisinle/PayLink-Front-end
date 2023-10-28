a18213baac8a052601f5a21a016f7c0f4b54864a
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-10-28 10:42:22 +0300

    setup with tailwindCss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-indigo-500 text-5xl flex justify-center">
+        React Vite with TailwingCSS
+      </h1>
+    </>
+  );
+}
+
+export default App;
