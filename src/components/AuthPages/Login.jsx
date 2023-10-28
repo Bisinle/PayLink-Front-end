3fad4664d74e40dee8a41b64c0270fe733b5784d
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-10-28 14:59:42 +0300

    added some form validation to login  using react-~form-hooks

--- src/components/AuthPages/Login.jsx
+++ src/components/AuthPages/Login.jsx
@@ -1,48 +1,51 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  // console.log(watch("username"));
+  // console.log(watch("password"));
+
+  function loginUser(data) {
+    console.log(data);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    // fetch("http://127.0.0.1:5555/auth/login", requestOptions)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data.access_token);
+    //     login(data.access_token);
+    //     data.access_token && navigate("/");
+    //     data.access_token ? setIsLogedin(true) : setIsLogedin(false);
+    //     localStorage.setItem("refresh_token", data.refresh_token);
+    //     localStorage.setItem("user_name", data.user_name);
+    //     localStorage.setItem("user_role", data.user_role);
+    //     localStorage.setItem("user_profile_pic", data.user_profile_pic);
+    //   });
+  }
   return (
     <section class="bg-gray-300  ">
       <div class="flex flex-col  w-full  items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
         <div class="w-full  bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl flex justify-center">
-              Create and account
+              Login
             </h1>
             <form class="space-y-4 md:space-y-6 " action="#">
-              <div>
-                <label
-                  for="firstname"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  First name
-                </label>
-                <input
-                  type="text"
-                  name="firstname"
-                  id="firstname"
-                  class="bg-gray-50 border border-gray-900 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-400 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="first name"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="Lastname"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  name="Lastname"
-                  id="Lastname"
-                  class="bg-gray-50 border border-gray-900 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-400 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Last name"
-                  required=""
-                />
-              </div>
               <div>
                 <label
                   for="username"
@@ -51,62 +54,32 @@ export default function Login() {
                   username
                 </label>
                 <input
-                  type="email"
+                  type="text"
                   name="username"
                   id="username"
                   class="bg-gray-50 border border-gray-900 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-400 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="user name"
-                  required=""
+                  {...register("username", {
+                    required: true,
+                    maxLength: 20,
+                    minLength: 5,
+                  })}
                 />
+
+                {errors.username && (
+                  <p style={{ color: "red" }}>
+                    {" "}
+                    <small>username is required</small>{" "}
+                  </p>
+                )}
+                {errors.username?.type === "maxLength" && (
+                  <p style={{ color: "red" }}>
+                    {" "}
+                    <small>should have max 25 characters</small>{" "}
+                  </p>
+                )}
               </div>
-              <div>
-                <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-900 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-400 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="address"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Address
-                </label>
-                <input
-                  type="email"
-                  name="address"
-                  id="address"
-                  class="bg-gray-50 border border-gray-900 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-400 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="address"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="phone number"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Password
-                </label>
-                <input
-                  type="number"
-                  name="phone number"
-                  id="phone number"
-                  placeholder="phone number"
-                  class="bg-gray-50 border border-gray-900 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-400 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
+
               <div>
                 <label
                   for="confirm-password"
@@ -116,12 +89,21 @@ export default function Login() {
                 </label>
                 <input
                   type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  {...register("password", { required: true, minLength: 8 })}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="password"
                 />
+                {errors.password && (
+                  <p style={{ color: "red" }}>
+                    <small>password is required</small>
+                  </p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p style={{ color: "red" }}>
+                    {" "}
+                    <small>should have min 2 characters</small>{" "}
+                  </p>
+                )}
               </div>
               <div class="flex items-start">
                 <div class="flex items-center h-5">
@@ -130,7 +112,6 @@ export default function Login() {
                     aria-describedby="terms"
                     type="checkbox"
                     class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
                   />
                 </div>
                 <div class="ml-3 text-sm">
@@ -146,18 +127,18 @@ export default function Login() {
                 </div>
               </div>
               <button
-                type="submit"
-                class="w-full text-white border  border-gray-300 bg-indigo-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                onClick={handleSubmit(loginUser)}
+                className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
               >
-                Create an account
+                Login
               </button>
               <p class="text-sm font-light  text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
+                Do not have an account?{" "}
                 <a
                   href="#"
                   class="font-medium text-primary-600 hover:underline dark:text-primary-500 text-indigo-800"
                 >
-                  Login here
+                  Signup here
                 </a>
               </p>
             </form>
