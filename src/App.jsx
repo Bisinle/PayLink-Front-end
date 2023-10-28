abe851afe95d3192fce16049070feef70d654ce8
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-10-28 11:18:16 +0300

    initial setup with tailwind and ReactVite

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-indigo-500 text-5xl flex justify-center">
+      <h1 className="text-white bg-indigo-500 text-5xl flex justify-center mt-5 w-54">
         React Vite with TailwingCSS
       </h1>
       <h1 className="text-white">React Vite with TailwingCSS</h1>
